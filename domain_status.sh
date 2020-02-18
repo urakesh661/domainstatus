72105779ae7dbd25f0cd55befac707614fd6e628
Rakesh Kirola <55341835+urakesh661@users.noreply.github.com>
2020-02-18 20:58:26 +0530

    Update domain_status.sh

--- domain_status.sh
+++ domain_status.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-start_time=$(date +%s)
-
 echo -n "Enter location of domains file: "
 read location
 
@@ -14,9 +12,16 @@ fi
 echo -n "Enter the response code which you would like to filter from the curl result:: "
 read response
 
+echo -n "Please provide connect-timeout[2]: "
+read connecttimeout
+
+echo -n "Please provide max-time[2]: "
+read maxtime
+
+start_time=$(date +%s)
 
 for i in $(cat $location);do
-	curl -o /dev/null  -s -w  "%{http_code} %{url_effective}\n" --connect-timeout 1 --max-time 1   $i | grep $response
+	curl -o /dev/null  -s -w  "%{http_code} %{url_effective}\n" --connect-timeout ${connecttimeout:-2} --max-time ${maxtime:-2}   $i | grep $response
 done
 
 end_time=$(date +%s)
